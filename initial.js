d1a3febc7528e6a9d7336ec255ba9b1d0c317dad
Husseini Husseini <36432010+hussein055@users.noreply.github.com>
2019-02-25 15:12:10 -0500

    Add files via upload

---
+++ initial.js
@@ -0,0 +1,8 @@
+var contact = document.querySelector(".nav_menu[href='#contact']");
+var contactForm = document.querySelector('#contact');
+
+contactForm.style.display = 'none';
+
+contact.addEventListener('click', function(){
+    
+} )
