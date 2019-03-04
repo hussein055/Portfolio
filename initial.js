75a4e47f917d08b9dbdbea129e4080a72231b183
Husseini Husseini <36432010+hussein055@users.noreply.github.com>
2019-03-03 19:19:25 -0500

    added contact form
    to open contact form click contact and click cancel button to cancel

--- initial.js
+++ initial.js
@@ -1,8 +1,18 @@
-var contact = document.querySelector(".nav_menu[href='#contact']");
+var contact = document.querySelector("[href='#contact']");
 var contactForm = document.querySelector('#contact');
-
-contactForm.style.display = 'none';
+var signIn = document.querySelector('.get_it');
+var cancelBtn = document.querySelector("input[value='cancel']");
+/*///////////////////////////
+click on contact to see the contact form 
+//////////////*/
+ contactForm.style.display = 'none';
 
 contact.addEventListener('click', function(){
+   contactForm.style.display = 'block'
+       
+} );
+
+cancelBtn.addEventListener('click', function() {
+    contactForm.style.display = 'none';
     
-} )
+})
