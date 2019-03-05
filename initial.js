251cd06f0d92f06546f0eb9055054b9effcb0560
Husseini Husseini <36432010+hussein055@users.noreply.github.com>
2019-03-04 19:58:16 -0500

    Add files via upload
    finally fixed the scrolling problems

--- initial.js
+++ initial.js
@@ -2,10 +2,12 @@ var contact = document.querySelector("[href='#contact']");
 var contactForm = document.querySelector('#contact');
 var signIn = document.querySelector('.get_it');
 var cancelBtn = document.querySelector("input[value='cancel']");
+var logInForm = document.querySelector('#sign_in');
 /*///////////////////////////
-click on contact to see the contact form 
+click on contact to see the contact form/ click cancel to exit the form
 //////////////*/
- contactForm.style.display = 'none';
+ contactForm.style.display = 'none'; 
+ logInForm.style.display = 'none';
 
 contact.addEventListener('click', function(){
    contactForm.style.display = 'block'
@@ -16,3 +18,11 @@ cancelBtn.addEventListener('click', function() {
     contactForm.style.display = 'none';
     
 })
+
+
+/* log in form  */
+
+signIn.addEventListener('click', function() {
+    logInForm.style.display = 'block';
+})
+
